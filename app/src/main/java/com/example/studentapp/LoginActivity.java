452cc9c2195373e9ea20c69870f0d75a67970681
Mohamed khedr <120657300+Mohamed-5der@users.noreply.github.com>
2023-10-24 15:24:01 +0300

    home and update sign up and login

--- app/src/main/java/com/example/studentapp/LoginActivity.java
+++ app/src/main/java/com/example/studentapp/LoginActivity.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 TextView signup,forget;
+Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         signup=findViewById(R.id.txt_signUp);
         forget=findViewById(R.id.txt_forget_pass);
+        login=findViewById(R.id.btn_login);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/studentapp/RegisterActivity.java
+++ app/src/main/java/com/example/studentapp/RegisterActivity.java
@@ -2,13 +2,79 @@ package com.example.studentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+ImageButton back;
 
+TextView gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        back =findViewById(R.id.btn_back);
+
+        gender =findViewById(R.id.txt_selectGender);
+        gender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContinueAsDialog();
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
+    public void showContinueAsDialog(){
+        final Dialog dialog = new Dialog(RegisterActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.gender_custom_dialog);
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+        dialog.getWindow().getAttributes().windowAnimations =R.style.DialoAnimation;
+        LinearLayout male =dialog.findViewById(R.id.l_male);
+        LinearLayout female =dialog.findViewById(R.id.l_female);
+        ImageButton close=dialog.findViewById(R.id.btn_close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dialog.dismiss();
+            }
+        });
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gender.setText(R.string.male);
+                dialog.dismiss();
+
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               gender.setText(R.string.female);
+                dialog.dismiss();
+            }
+        });
+    }
+
 }
