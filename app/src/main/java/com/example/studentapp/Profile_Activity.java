ad57dec40138043736997c6bbab424434ed8889a
mohamed-elbayomy <mohamedelbauomy616@gmial.com>
2023-11-06 19:51:30 +0200

    Contact_Activity

--- app/src/main/java/com/example/studentapp/Profile_Activity.java
+++ app/src/main/java/com/example/studentapp/Profile_Activity.java
@@ -1,16 +1,25 @@
 package com.example.studentapp;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile_Activity extends AppCompatActivity {
 
     RelativeLayout edit;
+    TextView gender;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -18,6 +27,13 @@ public class Profile_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         edit = findViewById(R.id.edit);
+        gender =findViewById(R.id.txt_selectGender);
+        gender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContinueAsDialog();
+            }
+        });
 
 
         edit.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +42,44 @@ public class Profile_Activity extends AppCompatActivity {
                 Intent intent = new Intent(Profile_Activity.this,Edit_Password_Activity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+    }
+
+    public void showContinueAsDialog(){
+        final Dialog dialog = new Dialog(Profile_Activity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.gender_custom_dialog);
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+        dialog.getWindow().getAttributes().windowAnimations =R.style.DialoAnimation;
+        LinearLayout male =dialog.findViewById(R.id.l_male);
+        LinearLayout female =dialog.findViewById(R.id.l_female);
+        ImageButton close=dialog.findViewById(R.id.btn_close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dialog.dismiss();
+            }
+        });
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gender.setText(R.string.male);
+                dialog.dismiss();
+
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gender.setText(R.string.female);
+                dialog.dismiss();
             }
         });
     }
